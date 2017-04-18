6be08b7fad1dcae7e5a617e70c5bd71198e49e09
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 17:34:33 -0400

    Reprepare for Heroku

--- message/app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class StaticPagesController < ApplicationController
-
-def root
-end
-
-end
--- message/frontend/reducers/root_reducer.js
+++
@@ -1,6 +0,0 @@
-import SessionReducer from './sessions_reducer';
-import { combineReducers } from 'redux';
-
-export default combineReducers({
-  sessions: SessionReducer
-});
