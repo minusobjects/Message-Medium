ac4748711710e4d88cff982339caa9d5256190fa
Michael Newton <mn@one-timepad.com>
2017-04-19 13:34:27 -0400

    Add route

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
 import RootReducer from '../reducers/root_reducer';
 
+const logger = createLogger();
+
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
