69b4df888d93fd6d0f09be40c1cc7d3977a281ab
Mike Newton <mikenewton@Mac-mini.local>
2017-04-26 11:42:03 -0400

    Add errors to story input

--- frontend/actions/story_actions.js
+++ frontend/actions/story_actions.js
@@ -2,6 +2,7 @@ import * as StoryAPIUtil from '../util/story_api_util';
 
 export const RECEIVE_ALL_STORIES = "RECEIVE_ALL_STORIES";
 export const RECEIVE_STORY = "RECEIVE_STORY";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
 export const receiveAllStories = stories => ({
   type: RECEIVE_ALL_STORIES,
@@ -13,26 +14,35 @@ export const receiveStory = story => ({
   story
 });
 
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
 export const fetchAllStories = (data) => dispatch => (
   StoryAPIUtil.storyIndex(data)
-    .then(stories => dispatch(receiveAllStories(stories)))
+    .then(stories => dispatch(receiveAllStories(stories)),
+    err => dispatch(receiveErrors(err.responseJSON)))
 );
 
 export const fetchStory = (storyId) => dispatch => (
   StoryAPIUtil.storyShow(storyId)
-    .then(story => dispatch(receiveStory(story)))
+    .then(story => dispatch(receiveStory(story)),
+    err => dispatch(receiveErrors(err.responseJSON)))
 );
 
 export const createStory = (story) => dispatch => {
 return (
   StoryAPIUtil.storyCreate(story)
-    .then(newStory => dispatch(receiveStory(newStory)))
+    .then(newStory => dispatch(receiveStory(newStory)),
+    err => dispatch(receiveErrors(err.responseJSON)))
   );
 };
 
 export const updateStory = (story) => dispatch => {
 return (
   StoryAPIUtil.storyUpdate(story)
-    .then(story => dispatch(receiveStory(story)))
+    .then(story => dispatch(receiveStory(story)),
+    err => dispatch(receiveErrors(err.responseJSON)))
   );
 };
